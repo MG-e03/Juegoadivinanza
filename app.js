4f7484eff9c0913632ef4bb385c805ec1ca17a74
MG-e03 <104379451+MG-e03@users.noreply.github.com>
2024-07-16 18:56:53 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+alert('Adivina el número secreto');
+
+// Agrega un console.log para verificar el valor de "intento" después de la entrada del usuario
+let intento = prompt('Elige un número entre 1 y 10');
+console.log('Valor de intento:', intento);
+
+let numeroSecreto = 5;
+
+
+// Agrega un console.log para verificar la comparación entre "intento" y "numeroSecreto"
+console.log('Resultado de la comparación:', intento == numeroSecreto);
+
+if (intento == numeroSecreto) {
+    alert('Te ganaste una noche de pasión ');
+} else {
+    // Agrega un console.log para verificar el valor de "numeroSecreto" cuando el jugador se equivoca
+    console.log('Valor del número secreto:', numeroSecreto);
+    alert('PERDISTE El número secreto era PERO BESAME :$ ' + numeroSecreto );
+}
+
+
+
